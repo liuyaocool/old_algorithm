6fc8c8a8f1ee537834a17e6a94b06c2b13a9801d
liuyao <liuyaocoool@163.com>
2021-04-26 21:49:42 +0800

    sort

--- src/main/java/prv/liuyao/alforithm/t002_datastruct/T02_Recursion.java
+++ src/main/java/prv/liuyao/alforithm/t002_datastruct/T02_Recursion.java
@@ -4,7 +4,7 @@ package prv.liuyao.alforithm.t002_datastruct;
  * 递归
  *  任何递归都一定可以改成非递归的方法 ✔️
  * 分析时间复杂的
- *  master公式 T(N)= a * T(N/b) + O(n^d);
+ *  master公式 T(N) = a * T(N/b) + O(n^d);
  *      a,b,d常数
  *      先决条件: 子规模 一致
  *          如 取数组最大值,分两半,左侧递归,右侧递归,左右等规模
