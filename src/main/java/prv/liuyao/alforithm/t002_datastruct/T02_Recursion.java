e93250f55eb229687c2b42a9cccb65726b4c4396
liuyao <liuyaocoool@163.com>
2021-04-26 21:04:42 +0800

    data struct

---
+++ src/main/java/prv/liuyao/alforithm/t002_datastruct/T02_Recursion.java
@@ -0,0 +1,17 @@
+package prv.liuyao.alforithm.t002_datastruct;
+
+/**
+ * 递归
+ *  任何递归都一定可以改成非递归的方法 ✔️
+ * 分析时间复杂的
+ *  master公式 T(N)= a * T(N/b) + O(n^d);
+ *      a,b,d常数
+ *      先决条件: 子规模 一致
+ *          如 取数组最大值,分两半,左侧递归,右侧递归,左右等规模
+ *      log_b-a < d -> O(N^d)
+ *      log_b-a > d -> O(N^(log_b-a))
+ *      log_b-a == d -> O(N^d * log_2-N)
+ *
+ */
+public class T02_Recursion {
+}
---
+++ src/main/java/prv/liuyao/alforithm/t003_sort/T01_Merge.java
@@ -0,0 +1,9 @@
+package prv.liuyao.alforithm.t003_sort;
+
+/**
+ * 归并排序
+ *
+ */
+public class T01_Merge {
+
+}
