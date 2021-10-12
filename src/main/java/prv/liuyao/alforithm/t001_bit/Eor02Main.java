f610fbd426d23f2626a88e30a3d84b53269fc21d
liuyao <liuyaocoool@163.com>
2021-10-13 00:23:20 +0800

    eor 题 加难度

--- src/main/java/prv/liuyao/alforithm/t001_bit/Eor02Main.java
+++ src/main/java/prv/liuyao/alforithm/t001_bit/Eor02Main.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import static prv.liuyao.alforithm.AlgorithmUtil.*;
 
 /**
- * a出现了k次, 其他都出现了m次, 且m>1,m>k, 找到a, k,m给定. 空间O(1), 时间O(n)
+ * a出现了k次, 其他都出现了m次, 且m>1,m>k, m给定, 找出a k. 空间O(1), 时间O(n)
  * 思路:
  *  1. 所有数 二进制位为1的 累加到 int[32] 中
  *  2. int[32]某一位 如果是m的整数倍,则说明 此位置a的二进制为是0, 不是整数倍 a而二进制位为1
@@ -33,22 +33,21 @@ public class Eor02Main {
         int minVal = -100;
         int maxVal = 100;
         int maxM = 40;
-        int loopTimes = 100000;
+        int loopTimes = 10000;
 
         for (int i = 0; i < loopTimes; i++) {
             int k = randomInt(1, maxM-1);
             int m = randomInt(k+1, maxM);
-
             int kTimeVal = randomInt(minVal, maxVal);
             int[] arr = randomArr(minVal, maxVal, kTimeVal, k, m);
 
             long tt = System.currentTimeMillis();
-            int res = findTimes(arr, k, m);
+            int[] res = findTimes(arr, m);
             t += System.currentTimeMillis() - tt;
 
-            if (kTimeVal != res){
-                System.out.println(kTimeVal);
-                System.out.println(res);
+            if (kTimeVal != res[0] || k != res[1]){
+                System.out.println(kTimeVal + ", " + res[0]);
+                System.out.println(k + ", " + res[1]);
                 System.out.println("结果错误");
             }
         }
@@ -92,22 +91,37 @@ public class Eor02Main {
         return result;
     }
 
+    /**
+     *
+     * @param arr
+     * @param k
+     * @param m
+     * @return
+     */
     // 思路 int[32] 保存每一个二进制位为1出现的次数
     // 如果 二进制位与m取模为0 则说明要找的那个数在此二进制位上为0
-    public static int findTimes(int[] arr, int k, int m) {
+    public static int[] findTimes(int[] arr, int m) {
         int binLen = 32;
         int[] t = new int[binLen];
+        int zeroTimes = 0;
         for (int num: arr) {
+            if (num == 0) {
+                zeroTimes++;
+            }
             for (int j = 0; j < binLen; j++) {
                 t[j] += num >> j & 1; // 收集二进制位
             }
         }
         int result = 0;
+        int k = 0;
         for (int i = 0; i < binLen; i++) {
-            if (t[i] % m != 0){
+            int i1 = t[i] % m;
+            if (i1 != 0){
                 result |= 1 << i; // | 有1为1
+                k = i1;
             }
         }
-        return result;
+        // 查找的数为0 是边界条件
+        return new int[]{result, result == 0 ? zeroTimes : k};
     }
 }
